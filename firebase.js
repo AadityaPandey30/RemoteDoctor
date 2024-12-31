c265621a52898782b5a094d7d80dcf2bf11aa202
Aaditya Pandey <125696438+AadityaPandey30@users.noreply.github.com>
2024-12-31 14:21:36 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
-// firebase.js
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-config.json');
 
-// Initialize Firebase
+// Parse the Firebase config from environment variables
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
-
 module.exports = db;
+
